cfbf7529fc78a6eb2a2f88847f6fe20376de14d0
ysthakur <45539777+ysthakur@users.noreply.github.com>
2023-06-08 09:40:41 -0400

    Parse strings

--- rust/src/de.rs
+++ rust/src/de.rs
@@ -1,7 +1,5 @@
-
-
-use serde::{Deserialize, forward_to_deserialize_any};
-use serde::de::{self, Visitor};
+use serde::de::{self, EnumAccess, MapAccess, SeqAccess, VariantAccess, Visitor};
+use serde::{forward_to_deserialize_any, Deserialize};
 
 use crate::error::{Error, Result};
 
@@ -62,6 +60,59 @@ impl<'de> Deserializer<'de> {
             }
         }
     }
+
+    fn parse_type(&mut self) -> Result<Option<String>> {
+        if self.input.starts_with("~") {
+            let _ = self.next()?;
+            let word = self.parse_str()?;
+            self.trim_whitespace()?;
+            Ok(Some(word))
+        } else {
+            Ok(None)
+        }
+    }
+
+    fn parse_str(&mut self) -> Result<String> {
+        match self.peek()? {
+            q @ ('"' | '\'') => {
+                // Normal quoted strings
+                // todo allow raw strings with r#""#
+                let mut res = String::new();
+                while !self.input.is_empty() {
+                    let c = self.next()?;
+                    if c == q {
+                        break;
+                    } else if c == '\\' {
+                        res.push(self.next()?);
+                    } else {
+                        res.push(c);
+                    }
+                }
+                Ok(res)
+            }
+            '`' => {
+                // Strings that extend to the end of the line
+                let str: String = self.input.chars().take_while(|&c| c != '\n').collect();
+                if str.is_empty() {
+                    Err(Error::Message("Expected a string, got nothing".to_string()))
+                } else {
+                    Ok(str)
+                }
+            }
+            _ => {
+                // Bare strings (single words)
+                let word: String = self.input.chars().take_while(|c| !c.is_whitespace()).collect();
+                if word.is_empty() {
+                    Err(Error::Message(
+                        "Expected a word, got whitespace".to_string(),
+                    ))
+                } else {
+                    self.input = &self.input[word.len()..];
+                    Ok(word)
+                }
+            }
+        }
+    }
 }
 
 impl<'de, 'a> de::Deserializer<'de> for &'a mut Deserializer<'de> {
@@ -69,24 +120,138 @@ impl<'de, 'a> de::Deserializer<'de> for &'a mut Deserializer<'de> {
 
     fn deserialize_any<V>(self, visitor: V) -> std::result::Result<V::Value, Self::Error>
     where
-        V: Visitor<'de> {
+        V: Visitor<'de>,
+    {
         self.trim_whitespace()?;
         if self.input.is_empty() {
             Err(Error::Eof)
-        } else if self.parse_keyword("true")? {
-            visitor.visit_bool(true)
-        } else if self.parse_keyword("false")? {
-            visitor.visit_bool(false)
-        } else if self.parse_keyword("null")? {
-            visitor.visit_unit()
         } else {
-            todo!()
+            let typ = self.parse_type()?;
+
+            if self.parse_keyword("true")? {
+                visitor.visit_bool(true)
+            } else if self.parse_keyword("false")? {
+                visitor.visit_bool(false)
+            } else if self.parse_keyword("null")? {
+                if let Some("None") = typ.as_deref() {
+                    visitor.visit_none()
+                } else {
+                    visitor.visit_unit()
+                }
+            } else if self.input.starts_with("[") {
+                if let Some("Some") = typ.as_deref() {
+                    visitor.visit_enum(self)
+                } else {
+                    visitor.visit_seq(self)
+                }
+            } else if self.input.starts_with("{") {
+                visitor.visit_map(self)
+            } else {
+                visitor.visit_string(self.parse_str()?)
+            }
         }
     }
 
     forward_to_deserialize_any! {
         bool i8 i16 i32 i64 i128 u8 u16 u32 u64 u128 f32 f64 char str string
-        bytes byte_buf option unit unit_struct newtype_struct seq tuple
-        tuple_struct map struct enum identifier ignored_any
+        bytes byte_buf option unit unit_struct newtype_struct seq tuple map identifier
+        struct tuple_struct ignored_any
+    }
+
+    fn deserialize_enum<V>(
+        self,
+        name: &'static str,
+        variants: &'static [&'static str],
+        visitor: V,
+    ) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        let typ = self.parse_type()?;
+    }
+}
+
+impl<'de, 'a> SeqAccess<'de> for &'a mut Deserializer<'de> {
+    type Error = Error;
+
+    fn next_element_seed<T>(&mut self, seed: T) -> Result<Option<T::Value>>
+    where
+        T: de::DeserializeSeed<'de>,
+    {
+        self.trim_whitespace()?;
+        if self.peek()? == ']' {
+            return Ok(None);
+        } else {
+            seed.deserialize(&mut **self).map(Some)
+        }
+    }
+}
+
+impl<'de, 'a> MapAccess<'de> for &'a mut Deserializer<'de> {
+    type Error = Error;
+
+    fn next_key_seed<K>(&mut self, seed: K) -> Result<Option<K::Value>>
+    where
+        K: de::DeserializeSeed<'de>,
+    {
+        self.trim_whitespace()?;
+        if self.peek()? == '}' {
+            return Ok(None);
+        } else {
+            seed.deserialize(&mut **self).map(Some)
+        }
+    }
+
+    fn next_value_seed<V>(&mut self, seed: V) -> Result<V::Value>
+    where
+        V: de::DeserializeSeed<'de>,
+    {
+        self.trim_whitespace()?;
+        if self.peek()? == '}' {
+            return Err(Error::Message("No value given".to_string()));
+        } else {
+            seed.deserialize(&mut **self)
+        }
+    }
+}
+
+impl<'de, 'a> EnumAccess<'de> for &'a mut Deserializer<'de> {
+    type Error = Error;
+    type Variant = Self;
+
+    fn variant_seed<V>(self, seed: V) -> Result<(V::Value, Self::Variant)>
+    where
+        V: de::DeserializeSeed<'de>,
+    {
+        todo!()
+    }
+}
+
+impl<'de, 'a> VariantAccess<'de> for &'a mut Deserializer<'de> {
+    type Error = Error;
+
+    fn unit_variant(self) -> Result<()> {
+        todo!()
+    }
+
+    fn newtype_variant_seed<T>(self, seed: T) -> Result<T::Value>
+    where
+        T: de::DeserializeSeed<'de>,
+    {
+        todo!()
+    }
+
+    fn tuple_variant<V>(self, len: usize, visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        todo!()
+    }
+
+    fn struct_variant<V>(self, fields: &'static [&'static str], visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        todo!()
     }
 }
